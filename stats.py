aee9f5401d51e10c28e45c12392dc2af98575920
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-03-14 20:08:59 -0700

    Add preliminary stats to front end.

--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 from app import app, db
 import datetime
+import random
 
 player_game_table = db.Table('player_game',
 	db.Column('player_id', db.Integer, db.ForeignKey('player.id')),
@@ -34,3 +35,22 @@ class Game(db.Model):
 	date = db.Column(db.Date)
 	resistance_rounds = db.Column(db.Integer)
 	spy_rounds = db.Column(db.Integer)
+
+def overall_stats(games):
+	# Just for kicks
+	def scanl(f, accum, it):
+		for item in it:
+			accum = f(accum, item)
+			yield accum
+
+	def accumulator(value, game):
+		(total, res_wins, spy_wins) = value
+		total = total + 1
+		if game.resistance_rounds >= 3:
+			res_wins = res_wins + 1
+		else:
+			spy_wins = spy_wins + 1
+		
+		return (total, res_wins, spy_wins)
+
+	return list(scanl(accumulator, (0, 0, 0), games))
--- views.py
+++ views.py
@@ -1,6 +1,29 @@
 from app import app, db
 from flask import render_template, flash, redirect, session, url_for, request
+from datetime import datetime
+import stats
+from stats import Player, Game, overall_stats
+
+@app.route('/games/')
+def games():
+	pass
+
+@app.route('/leaderboards/')
+def leaderboards():
+	pass
+
+@app.route('/search/')
+def search():
+	pass
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	games = Game.query.order_by(Game.date).all()
+	players = Player.query.all()
+
+	overall_stats = stats.overall_stats(games)
+
+	return render_template('index.html',
+		overall_stats=overall_stats,
+		games=games,
+		players=players)
